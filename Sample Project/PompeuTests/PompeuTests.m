1588d9593174bd6077cc953c61edc482d96b9c10
Paolo Tagliani <pablosproject@gmail.com>
2015-06-04 12:03:42 +0200

    Added test on single view controller

--- Sample Project/PompeuTests/PompeuTests.m
+++ Sample Project/PompeuTests/PompeuTests.m
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "PompeuTestViewController.h"
+#import "UIView+Pompeu.h"
+#import "UIViewController+Pompeu.h"
 
 @interface PompeuTests : XCTestCase
 
@@ -15,26 +18,42 @@
 
 @implementation PompeuTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testViewController
+{
+    PompeuTestViewController *testController = [[PompeuTestViewController alloc] initWithNibName:@"PompeuTestViewController" bundle:[NSBundle bundleForClass:[PompeuTestViewController class]]];
+    
+    XCTAssertNotNil(testController.view);
+    
+    //Execute the test after tha actual runloop since Pompeu is updating the view after that
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.00 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        XCTAssertTrue([testController.testLabel.text isEqualToString:@"label text"]);
+        
+        XCTAssertTrue([[testController.testButton titleForState:UIControlStateNormal] isEqualToString:@"default button title"]);
+        XCTAssertTrue([[testController.testButton titleForState:UIControlStateHighlighted] isEqualToString:@"highlighted button title"]);
+        XCTAssertTrue([[testController.testButton titleForState:UIControlStateSelected] isEqualToString:@"selected button title"]);
+        XCTAssertTrue([[testController.testButton titleForState:UIControlStateDisabled] isEqualToString:@"disabled button title"]);
+        
+        XCTAssertTrue([testController.testTextView.text isEqualToString:@"text view text"]);
+        
+        XCTAssertTrue([testController.testTextField.text isEqualToString:@"text field text"]);
+        
+        XCTAssertTrue([[testController.testSegmentedControl titleForSegmentAtIndex:0] isEqualToString:@"first segment title"]);
+        XCTAssertTrue([[testController.testSegmentedControl titleForSegmentAtIndex:1] isEqualToString:@"second segment title"]);
+        XCTAssertTrue([[testController.testSegmentedControl titleForSegmentAtIndex:2] isEqualToString:@"third segment title"]);
+    });
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+
 
 @end
