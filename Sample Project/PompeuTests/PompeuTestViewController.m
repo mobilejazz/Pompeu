7ba5d684bd2061108a1b4cae74002fb9acfc7c56
Paolo Tagliani <pablosproject@gmail.com>
2015-06-04 12:02:24 +0200

    Added localization file and test view controller

---
+++ Sample Project/PompeuTests/PompeuTestViewController.m
@@ -0,0 +1,23 @@
+//
+//  PompeuTestViewController.m
+//  Pompeu
+//
+//  Created by Paolo Tagliani on 6/4/15.
+//  Copyright (c) 2015 Mobile Jazz. All rights reserved.
+//
+
+#import "PompeuTestViewController.h"
+#import "UIViewController+Pompeu.h"
+
+@interface PompeuTestViewController ()
+
+@end
+
+@implementation PompeuTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self pmp_localizate];
+}
+
+@end
